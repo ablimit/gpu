0b139456f2b119c182ec5f00d27bc7438e6fb7a4
Ablimit Aji <aaji@mathcs.emory.edu>
2012-11-25 21:30:28 -0500

    DB2 required header files are added

--- example/example.c
+++ example/example.c
@@ -1,5 +1,12 @@
+
 #include<stdio.h>
+// following headers are required for UDF
 #include<sqludf.h>
+#include<sql.h>
+#include<sqlda.h>
+#include<sqlca.h>
+#include<memory.h>
+
 
 void SQL_API_FN example(
 	SQLUDF_VARCHAR *poly,
